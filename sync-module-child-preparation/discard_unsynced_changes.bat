caca08b947f1de02fae3cf661b6e7e2151944c5a
Christian Neumann <x-ian@x-ian.net>
2013-02-16 17:41:02 +0100

    misc

--- sync-module-child-preparation/discard_unsynced_changes.bat
+++ sync-module-child-preparation/discard_unsynced_changes.bat
@@ -5,5 +5,5 @@ echo Only invoke this if you are requested to do so!
 echo.
 echo Press Ctrl-C to abort or
 pause
-mysql -u openmrs -ppa55ionFruit openmrs -e "delete from sync_record;"
+mysql -u openmrs -pPW openmrs -e "delete from sync_record;"
 pause
