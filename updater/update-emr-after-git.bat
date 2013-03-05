05f70f3278a8c34569705319ae284bcccb8ceecc
unknown <@x-ian@x-ian.net>
2013-03-05 02:02:03 +0100

    cleanup of tomcat stuff

--- updater/update-emr-after-git.bat
+++ updater/update-emr-after-git.bat
@@ -1,5 +1,6 @@
 @echo OFF
 
+echo.
 echo stop openmrs
 set PWD=%CD%
 cd %TOMCAT_HOME%\bin 2>NUL
@@ -7,15 +8,19 @@ call shutdown.bat
 cd /d %PWD%
 rem cls
 
+echo.
 echo backup current database
 mysqldump -u root -proot openmrs > backup\openmrs-%HOSTNAME%.sql"
 
+echo.
 echo backup current modules
 move "%OPENMRS_HOME%\modules\*.omod" backup\
 
+echo.
 echo update modules
 copy ..\modules\*.omod "%OPENMRS_HOME%\modules"
 
+echo.
 echo update metadata
 mysql -u root -proot openmrs < ..\metadata\dump_addresshierarchy.sql
 mysql -u root -proot openmrs < ..\metadata\dump_concepts.sql
@@ -27,6 +32,14 @@ mysql -u root -proot openmrs < ..\metadata\dump_reports.sql
 mysql -u root -proot openmrs < ..\metadata\dump_scheduler.sql
 mysql -u root -proot openmrs < ..\metadata\dump_users.sql
 
+echo.
+echo cleanup tomcat stuff
+rd /q /s "%TOMCAT_HOME%\work\"
+mkdir "%TOMCAT_HOME%\work\"
+rd /q /s "%TOMCAT_HOME%\temp\"
+mkdir "%TOMCAT_HOME%\temp\" 
+
+echo.
 echo misc
 
 echo.
