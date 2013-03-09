2873ee94f639f890e02001e70c43fdab0ad438bc
unknown <x-ian@x-ian.net>
2013-03-09 12:18:31 -0500

    windows compliant

--- misc/delete_all_patients.bat
+++ misc/delete_all_patients.bat
@@ -11,16 +11,6 @@ pause
 echo.
 echo Press Ctrl-C to abort or
 pause
-mysql -u root -pGot2LuvYa openmrs -e "CREATE TEMPORARY TABLE patient_ids AS (SELECT patient_id FROM patient);"
-mysql -u root -pGot2LuvYa openmrs -e "delete from name_phonetics where person_name_id in (select person_name_id from person_name where person_id in (select patient_id from patient_ids));"
-mysql -u root -pGot2LuvYa openmrs -e "delete from person_name where person_id in (select patient_id from patient_ids);"
-mysql -u root -pGot2LuvYa openmrs -e "delete from person_attribute where person_id in (select patient_id from patient_ids);"
-mysql -u root -pGot2LuvYa openmrs -e "delete from person_address where person_id in (select patient_id from patient_ids);"
-mysql -u root -pGot2LuvYa openmrs -e "delete from obs;"
-mysql -u root -pGot2LuvYa openmrs -e "delete from encounter_provider;"
-mysql -u root -pGot2LuvYa openmrs -e "delete from encounter;"
-mysql -u root -pGot2LuvYa openmrs -e "delete from patient where patient_id in (select patient_id from patient_ids);"
-mysql -u root -pGot2LuvYa openmrs -e "delete from person where person_id in (select patient_id from patient_ids);"
-mysql -u root -pGot2LuvYa openmrs -e "DROP TABLE patient_ids;"
+mysql -u root -pGot2LuvYa openmrs -e "CREATE TEMPORARY TABLE patient_ids AS (SELECT patient_id FROM patient); delete from name_phonetics where person_name_id in (select person_name_id from person_name where person_id in (select patient_id from patient_ids)); delete from person_name where person_id in (select patient_id from patient_ids); delete from person_attribute where person_id in (select patient_id from patient_ids); delete from person_address where person_id in (select patient_id from patient_ids); delete from obs; delete from encounter_provider; delete from encounter; delete from patient where patient_id in (select patient_id from patient_ids); delete from person where person_id in (select patient_id from patient_ids); DROP TABLE patient_ids;"
 
 pause
