c76983cb5fa1b0e9e7b6a99fcfecdad96f4e851d
Christian Neumann <x-ian@x-ian.net>
2013-05-04 11:12:09 +0200

    delete sync modules stuff

--- sync-module-child-preparation/discard_unsynced_changes.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo.
-echo This will delete all local, unsynchronized changes from OpenMRS.
-echo Only invoke this if you are requested to do so!
-echo.
-echo Press Ctrl-C to abort or
-pause
-mysql -u openmrs -pPW openmrs -e "delete from sync_record;"
-pause
