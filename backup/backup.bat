b91f40095ef1a04a0ab0dc5b6727d04bd06a0b52
Christian Neumann <x-ian@x-ian.net>
2013-05-26 20:50:47 +0200

    more foolproof backup

--- backup/backup.bat
+++ backup/backup.bat
@@ -47,8 +47,8 @@ cls
 
 echo Step 1: Stop OpenMRS localhost
 
-echo Step 2: Delete old backups
-mkdir %BACKUP-DRIVE%:\%BACKUP-DIR%
+echo Step 2: Delete old backup with same name/timestamp
+mkdir %BACKUP-DRIVE%:\%BACKUP-DIR% >NUL
 rd /q /s "%BACKUP-PATH%" 2>NUL
 mkdir "%BACKUP-PATH%"
 
@@ -57,11 +57,10 @@ mysqldump -u root -pGot2LuvYa openmrs > openmrs.sql
 
 echo Step 4: Compress and encrypt database
 7za a -pGot2LuvYa database-dump.zip openmrs.sql >NUL
+del openmrs.sql
+move database-dump.zip "%EMR_HOME%" >NUL
 
-echo Step 5: Copy database to flash
-copy database-dump.zip "%BACKUP-PATH%" >NUL
-
-echo Step 6: Cleanup
+echo Step 5: Cleanup
 rem hack to wait for 30 secs to give tomcat time to really shut down
 ping -n 30 127.0.0.1 >nul
 rem Apache temp stuff, dump files, ...
@@ -71,11 +70,8 @@ rd /q /s "%TOMCAT_HOME%\work" 2>NUL
 mkdir "%TOMCAT_HOME%\work"
 rd /q /s "%TOMCAT_HOME%\webapps\openmrs" 2>NUL
 rd /q /s "..\..\database-dump" 2>NUL
-mkdir "..\..\database-dump"
-del openmrs.sql
-del database-dump.zip
 
-echo Step 7: Copy runtime environment to flash
+echo Step 6: Copy runtime environment to flash
 xcopy /e /c /q /y /h "%EMR_HOME%" "%BACKUP-PATH%" >NUL
 
 echo.
